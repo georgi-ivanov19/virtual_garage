c99b2fc75db59889152c81fe4bfe0a2e35d2f5b0
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2020-11-17 00:32:17 +0200

    first commit

---
+++ db/migrate/20201116174643_create_cars.rb
@@ -0,0 +1,13 @@
+class CreateCars < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.string :engine
+      t.string :transmission
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
