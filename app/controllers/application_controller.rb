5451fed0a1589fc074013b19db37ecc440102495
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2020-11-27 01:24:34 +0200

    Added username to user my cars turned into my garage and made it a little different from the index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    #protect the database, while allowing these fields to be updated
+    protected
+    def configure_permitted_parameters        
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
---
+++ db/migrate/20201126203238_add_username_to_users.rb
@@ -0,0 +1,7 @@
+class AddUsernameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
