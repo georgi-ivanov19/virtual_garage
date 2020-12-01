32d1c78f20f49862ecf9d660b0df57bd9e66b334
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2020-12-01 20:06:20 +0200

    added validation to image upload

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,13 +3,21 @@ class Car < ApplicationRecord
     has_many :comments
     has_many_attached :images
     validates :make, :model, :description, :transmission, :engine,:user_id, presence: true
+    validate :image_type
 
    def thumbnail input
     return self.images[input]
-   end
-#    def small_thumbnail input
-#     return self.images[input].variant(resize: '120x68!').processed
-#    end
+   end    
 
-     
+   private
+   def image_type
+    if images.attached? == false
+        errors.add(:images, "are missing!")
+        end
+        images.each do |image|
+            if !image.content_type.in?(%('image/jpeg image/png'))
+                errors.add(:images, "can only have JPEG of PNG extensions")
+            end
+        end
+    end
 end
