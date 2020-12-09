a9e4be37b76f0557aa9e6995384d2bfc67edb2bb
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2020-12-09 16:01:12 +0200

    disabled sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,14 +94,16 @@ Rails.application.configure do
   config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
 
   config.action_mailer.default_url_options = {host: 'http://virtualgaragee.herokuapp.com'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
     :enable_starttls_auto => true
   }
 end
