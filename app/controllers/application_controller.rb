56661e3d4bca5459843721115d5c7b0755229ce0
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2020-12-09 20:17:00 +0200

    added delete functionality to comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, except: [:index]
+    protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     #protect the database, while allowing these fields to be updated
