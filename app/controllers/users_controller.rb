631358ebb0a7294d030b87bd1b57784631aca1f0
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2020-12-12 12:53:37 +0200

    added jquery validate email and username already taken check

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < Devise::RegistrationsController
+    #controller ised to define methods that check if email and username are already in the db (taken)
+    def check_email
+        @user = User.find_by_email(params[:user][:email])
+      
+        respond_to do |format|
+        format.json { render :json => !@user }
+        end
+    end
+
+    def check_username
+        @user = User.find_by_username(params[:user][:username])
+      
+        respond_to do |format|
+        format.json { render :json => !@user }
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,15 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+
+    #methods to check if email and username are taken using AJAX
+    get "check_email" => "users#check_email", as: :check_email
+    get "check_username" => "users#check_username", as: :check_username
   end
 
-  resources :cars
+  
+
+  resources :cars 
   root 'cars#index'
   get '/search', to: 'cars#search'
   get '/garage/:id', to: 'cars#garage', as: 'garage'
